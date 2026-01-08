526a1b4e7d20a82ce78fa41b60df4a2987b9e456
Angel S. Moreno <angelxmoreno@gmail.com>
2026-01-08 15:18:06 -0500

    chore: added dev tooling

---
+++ .remarkrc.mjs
@@ -0,0 +1,8 @@
+export default {
+    plugins: [
+        'remark-preset-lint-recommended', // base rules
+        ['remark-lint-maximum-line-length', false],
+        ['remark-lint-no-dead-urls', false],
+        ['remark-lint-no-literal-urls', true],
+    ],
+};
